d5d5e505f23a22e051b06f65340f62ad05cea874
Atakan <merata9399@gmail.com>
2022-04-01 22:21:13 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,63 @@
+# python main.py -m screen:ipad
+# Yapılacaklar: global stopflag'i self.variable yap.
+
+from kivy.app import App
+from kivy.uix.screenmanager import Screen
+from kivy.uix.image import Image
+from kivy.graphics.texture import Texture
+from kivy.clock import Clock
+import requests
+import time
+import threading
+from kivy.properties import StringProperty
+from datetime import datetime
+import socket
+
+
+class MainPage(Screen):
+    first = True
+    second = True
+
+    def tcp(self, *args):
+        y = threading.Thread(target=self.tcpthread, daemon=True)  # Setup thread
+        y.start()  # Starts thread
+
+    def tcpthread(self):
+        global s
+        host = self.box.text
+        port = 10001
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect((host, port))
+            # s.sendall(b'Hello, world')
+            while True:
+                # s.sendall(b'Hello, world')
+                data = s.recv(1024)
+                datastr = data.decode("utf-8")
+
+    def test1(self):
+        if self.first is True:
+            s.send(b'a')
+            print('a')
+            self.first = False
+        elif self.first is False:
+            s.send(b'b')
+            print('b')
+            self.first = True
+
+    def test2(self):
+        if self.second is True:
+            s.send(b'c')
+            print('c')
+            self.second = False
+        elif self.second is False:
+            s.send(b'd')
+            print('d')
+            self.second = True
+
+
+class MyApp(App):
+    pass
+
+
+if __name__ == '__main__':
+    MyApp().run()
